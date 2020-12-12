cf64335ea628e6da7584e34a925357b3c79c0f92
Reinhard-P <Reinhard.Pfaff@t-online.de>
2020-12-12 10:13:53 +0100

    AoC-12 Part 1 solved

---
+++ Reinhard/AoC-12/src/AoC-12.cpp
@@ -0,0 +1,166 @@
+﻿/**
+ * @file AoC-12.cpp
+ * @author Reinhard Pfaff
+ * @brief AoC Puzzle 2020 #12
+ * @version 0.1
+ * @date 2020-12-12
+ * 
+ * @copyright Copyright (c) 2020 Reinhard Pfaff
+ * 
+ */
+
+#include "AoC.h"
+using namespace std;
+
+#include "AoC-Tools.h"
+
+
+// Function declarations
+// =====================
+int Puzzle_12(vector <string> &sList, int iPuzzle);
+
+
+/**
+ * @brief main (what else?)
+ * 
+ * @return int 1 - Error
+ */
+int main()
+{
+    cout << MakeHeadline("Advent of Code 2020 Puzzle # 12",'=');
+
+    // Reading input strings
+    vector<string> vs;
+    if (ReadStrings("input.txt", vs) < 0)
+    {
+        cout << "---ERROR Reading strings. Aborting!" << endl;
+        return 1;
+    }   
+
+    // first puzzle of day 
+    Puzzle_12(vs,1);
+
+    // Second puzzle of the day
+    // Puzzle_12(vs,2);
+
+    return 0;
+}
+
+
+
+
+/**
+ * @brief           AoC Puzzle
+ * 
+ * Part 1:
+ * - Find Manhatten Distance to a list of navigation commands:
+ * 
+ * The navigation instructions (your puzzle input) consists of a sequence of single-character actions paired with integer input values. After staring at them for a few minutes, you work out what they probably mean:
+
+    Action N means to move north by the given value.
+    Action S means to move south by the given value.
+    Action E means to move east by the given value.
+    Action W means to move west by the given value.
+    Action L means to turn left the given number of degrees.
+    Action R means to turn right the given number of degrees.
+    Action F means to move forward by the given value in the direction the ship is currently facing.
+    The ship starts by facing east. Only the L and R actions change the direction the ship is facing. (That is, if the ship is facing east and the next instruction is N10, the ship would move north 10 units, but would still move east if the following action were F.)
+
+    For example:
+
+    F10
+    N3
+    F7
+    R90
+    F11
+
+ * 
+ * @param vector<string>   list of strings
+ * @return int             0
+ */
+
+int Puzzle_12(vector<string> &sList, int iPuzzle)
+{
+    if (iPuzzle == 1)
+        cout << MakeHeadline("Advent of Code 2020 Puzzle # 12 Part 1",'=');
+    else
+        cout << MakeHeadline("Advent of Code 2020 Puzzle # 12 Part 2",'=');        
+
+    int iLine = 0;
+
+    // astronomical system (south = 0 Azimuth)
+    int cx = 0;             // + -> North;  - -> South
+    int cy = 0;             // + -> East;   - -> West
+    int heading = 270;      // 0 = south; 90 = west; 180 = north; 270 = east
+
+    char cmd;
+    int dist;
+    
+    for (string ts: sList)
+    {
+        iLine += 1;
+
+        // Decoding command and dist
+        stringstream sst(ts);
+        sst >> cmd >> dist;
+
+        // cout << "-Moving form: " << cx << "," << cy <<" head= " << heading << " cmd: " << cmd << " dist= " << dist ; 
+
+        // lets do it
+        switch (cmd)
+        {
+            case 'N':
+                cy += dist;
+                break;
+                
+            case 'S':
+                cy -= dist;
+                break;
+
+            case 'W':
+                cx -= dist;
+                break;
+
+            case 'E':
+                cx += dist;
+                break;
+
+            case 'L':
+                heading -= dist;
+                while (heading < 0)
+                    heading += 360;
+                break;
+
+            case 'R':
+                heading += dist;
+                while (heading >= 360)
+                    heading -= 360;
+                break;
+
+            case 'F':
+                switch (heading)
+                {
+                    case   0: cy -= dist; break;
+                    case  90: cx -= dist; break;
+                    case 180: cy += dist; break;
+                    case 270: cx += dist; break;
+                    default:
+                        cout << " Error in heading : " << heading << endl;
+                }
+                break;
+
+            default:
+                cout << " Error in Command : " << iLine << " :: "<< ts <<  " :: " << endl;
+        }
+
+    //    cout << " to: " << cx << "," << cy <<" head= " << heading << endl; 
+
+    }
+
+    int manhatten = abs(cx) + abs(cy);
+
+    cout << " Manhattendistance = " << manhatten << endl;
+
+    return 0;
+}
+
---
+++ Reinhard/AoC-12/src/AoC-Tools.cpp
@@ -0,0 +1,337 @@
+/**
+ * @file AoC-Tools.cpp
+ * @author Reinhard Pfaff
+ * @brief Collection of tools for Advent of Code Puzzles
+ * @version 0.1
+ * @date 2020-12-03
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+#include "AoC.h"
+using namespace std;
+
+#include "AoC-Tools.h"
+
+
+/**
+ * @brief Generate a std::string with three lines, containing a "framed" head-string
+ * 
+ * looks like:
+ * 
+ * ========
+ * = head =
+ * ========
+ * 
+ * @param head          string to be framed
+ * @param char_frame    charachter used for framing (i.e. '*','-','=',...)
+ * @return string       three line string
+ */
+string MakeHeadline(const string head, const char char_frame)
+{
+    stringstream ts;
+    int ls = head.length() + 4;
+    ts << endl;
+    ts << string(ls,char_frame) << endl;
+    ts << char_frame << " " << head << " " << char_frame << endl;
+    ts << string(ls,char_frame) << endl;
+    ts << endl;
+    return ts.str();
+}
+
+/**
+ * @brief Read Strings from file and store them in a vector<string>
+ * 
+ * the stings are appended to the given vector<string>
+ * 
+ * @param fn    - filename
+ * @param v_s   - vector<string> to store 
+ * @return int  - errorcode oder numer of strings
+ *              -1 ERROR: file open error
+ *              >= 0 Number of read lines
+ */
+int ReadStrings(const string fn, vector<string>& v_s)
+{
+    int count = 0;
+    ifstream fil(fn);
+
+    // open the input file
+    if (!fil.is_open())
+    {
+        cout << "Could not open file: [" << fn << "]" << endl;
+        return -1;
+    }
+
+    // read all lines
+    string line;
+    while (getline(fil, line))
+    {
+        v_s.push_back(line);
+        count++;
+    }
+    return count;
+}
+
+/**
+ * @brief Read Strings from file and store them in a vector<string>
+ * 
+ * the stings are appended to the given vector<string>
+ * 
+ * Remark:
+ * - Strings for Puzzle 04 are spread over more than one line
+ * - Empty lines are string delimiters: 
+ *   For example:
+ *       <BOF>
+ *       ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+ *       byr:1937 iyr:2017 cid:147 hgt:183cm
+ *       
+ *       iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+ *       hcl:#cfa07d byr:1929
+ *       
+ *       hcl:#ae17e1 iyr:2013
+ *       eyr:2024
+ *       ecl:brn pid:760753108 byr:1931
+ *       hgt:179cm
+ *       <EOF>
+ *   results in a vector w/ three strings
+ * 
+ * @param fn    - filename
+ * @param v_s   - vector<string> to store 
+ * @return int  - errorcode oder numer of strings
+ *              -1 ERROR: file open error
+ *              >= 0 Number of read lines in file (incl. empty lines)
+ */
+int ReadStrings4P04(const string fn, vector<string>& v_s)
+{
+    int count = 0;
+    ifstream fil(fn);
+
+    // open the input file
+    if (!fil.is_open())
+    {
+        cout << "Could not open file: [" << fn << "]" << endl;
+        return -1;
+    }
+
+    // read all lines
+    string line;
+    string cline;   // compount line
+    while (getline(fil, line))  // line delimiters are NOT stored
+    {
+        if (line.length() > 0)
+        {
+            // append the current read line to the cline (don't forget a separator)
+            cline += line;
+            cline += " ";
+        }
+        else
+        {
+            // this is a new line, so push back the collected strings
+            v_s.push_back(cline);
+            // and prepare the cline
+            cline.clear();
+        }
+        count++;
+    }
+    // maybe there was no empty line at the end of the file
+    if (cline.length() > 0)
+    {
+        v_s.push_back(cline);
+    }
+    return count;
+}
+
+/**
+ * @brief Read Strings from file and store them in a vector<string>
+ * 
+ * the stings are appended to the given vector<string>
+ * 
+ * Remark:
+ * - Strings are spread over more than one line
+ * - Empty lines are string delimiters: 
+ *   For example:
+ *       <BOF>
+ *       ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+ *       byr:1937 iyr:2017 cid:147 hgt:183cm
+ *       
+ *       iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+ *       hcl:#cfa07d byr:1929
+ *       
+ *       hcl:#ae17e1 iyr:2013
+ *       eyr:2024
+ *       ecl:brn pid:760753108 byr:1931
+ *       hgt:179cm
+ *       <EOF>
+ *   results in a vector w/ three strings. The new string is optionaly separated by sep
+ * 
+ * @param fn    - filename
+ * @param v_s   - vector<string> to store 
+ * @param sep   - separator
+ * @return int  - errorcode oder numer of strings
+ *              -1 ERROR: file open error
+ *              >= 0 Number of read lines in file (incl. empty lines)
+ */
+int ReadMultilineStrings(const string fn, vector<string>& v_s, const string sep)
+{
+    int count = 0;
+    ifstream fil(fn);
+
+    // open the input file
+    if (!fil.is_open())
+    {
+        cout << "Could not open file: [" << fn << "]" << endl;
+        return -1;
+    }
+
+    // read all lines
+    string line;
+    string cline;   // compount line
+    while (getline(fil, line))  // line delimiters are NOT stored
+    {
+        if (line.length() > 0)
+        {
+            // append the current read line to the cline (don't forget the optinal separator)
+            cline += line;
+            if (sep.length() > 0)
+                cline += sep;
+        }
+        else
+        {
+            // this is a new line, so push back the collected strings
+            v_s.push_back(cline);
+            // and prepare the cline
+            cline.clear();
+        }
+        count++;
+    }
+    // maybe there was no empty line at the end of the file
+    if (cline.length() > 0)
+    {
+        v_s.push_back(cline);
+    }
+    return count;
+}
+
+/**
+ * @brief explode 
+ * 
+ * * converts a string in a vector<string>
+ * 
+ * @param s     - string to "explode"
+ * @param sep   - string which contains separators (more than one allowed)
+ * @return const vector<string> 
+ */
+const vector<string> explode(const string& s, const string sep)
+{
+	string buff{""};
+	vector<string> v;
+	
+	for(auto n:s)
+	{
+        size_t pos = sep.find(n);
+		if(string::npos == pos)
+        {
+            buff += n;
+        }
+        else
+        {
+            // char in sep!
+		    if(pos >= 0 && buff != "") 
+            { 
+                v.push_back(buff); 
+                buff = ""; 
+            }
+        }
+	}
+
+	if(buff != "") 
+        v.push_back(buff);
+	
+	return v;
+}
+
+/**
+ * @brief Counting Characters in Vector of strings
+ * 
+ * @param sList     - vector<string>
+ * @param cX        - character to search and count for
+ * @return int      - total number of characters in vector<string>
+ */
+int count_cX(vector<string> &sList, char cX)
+{
+    int iCount = 0;
+    for (string ts: sList)
+    {
+        for (char tc : ts)
+        {
+            if (tc == cX)
+                iCount += 1;
+        }
+    }
+
+    return iCount;
+}
+
+/**
+ * @brief counting characters around pos j in string i
+ * 
+ * @param sa        - vector<string> &sa
+ * @param sb        - vector<string> &sb
+ * @param ii        - Index in Vector
+ * @param jj        - Index in string
+ * @param tc        - character to count for
+ * @param iMethod   - 1- search 8 neighbors; 2- search in 8 directions
+ * @return int      - number ob char in the 8 places around i,j
+ */
+int count_surrounding(vector<string> &sa, int ii, int jj, char tc, int iMethod)
+{
+    int iCount = 0;
+
+    // Method 2: Search directions
+    for (int i = -1; i <= +1; i++)
+    {
+        for (int j = -1; j <= +1; j++)
+        {
+            // Now look in direction n*j + n*j
+            if ((i==0) && (j==0))
+            {
+                continue; // do nothing at my current position
+            }
+
+            // go in this direction (i,j)
+            int delta = 1;
+            while (true)
+            {
+                int ci = ii + delta * i;
+                int cj = jj + delta * j;
+
+                // within bounds?
+                if (ci < 0 || ci >= (sa.size()))
+                    break;
+
+                if (cj < 0 || cj >= (sa[ii].size()))
+                    break;
+                
+                // character found ?
+                if (sa[ci][cj] == tc)
+                {
+                    iCount++;
+                    break;
+                }
+
+                if (iMethod == 1)
+                    break;
+
+                // One step further, only if the place is empty
+                if (sa[ci][cj] != '.')
+                    break;
+
+                // OK, move on in the current direction
+                delta++;
+            }
+        }
+    }
+    return iCount;
+}
+
